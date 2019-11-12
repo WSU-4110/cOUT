948d5b625c6af983201375e16dd4e7d0f13e7843
taufiqa <41548419+taufiqa@users.noreply.github.com>
2019-11-12 11:51:48 -0500

    Implemented the Singleton Design Pattern for DBManager

--- webfiles_coutproject/WebContent/DbManager.java
+++ webfiles_coutproject/WebContent/DbManager.java
@@ -2,19 +2,41 @@ package aaa;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 public class DbManager {
-public Connection getConnection()
-{
-	try{
-	Class.forName("com.mysql.jdbc.Driver");
-	Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cout", "root", "23paddock");
-	return conn;
-	} catch (ClassNotFoundException e){
-		e.printStackTrace();
-		return null;
-	} catch(SQLException e){
-		e.printStackTrace();
-		return null;
+	
+	private static DbManager instance;
+	private Connection connection;
+	private String url = "jdbc:mysql://localhost:3306/cout";
+	private String userName = "root";
+	private String password = "23paddock";
+	
+	private DbManager() throws SQLException {
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			this.connection = DriverManager.getConnection(url, userName, password);
+		}catch (ClassNotFoundException e){
+			e.printStackTrace();
+			return null;
+		} catch(SQLException e){
+			e.printStackTrace();
+			return null;
+		}
+		
 	}
-}
+	public Connection getConnection(){
+		return connection;	
+	}
+
+	public static DbManager getInstance() throws SQLException {
+		if (instance == null) {
+			instance = new DbManager();
+		} 
+		else if (instance.getConnection().isClosed()) {
+			instance = new DbManager();
+		}
+		
+		return instance;
+	}
 }
