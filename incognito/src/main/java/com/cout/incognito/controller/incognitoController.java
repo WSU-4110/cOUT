4d4990ff02bc5c228187c4e1dd8d57e23d272981
taufiqa <anika.taufiq@wayne.edu>
2019-12-01 19:39:31 -0500

    updating incognito controller

--- incognito/src/main/java/com/cout/incognito/controller/incognitoController.java
+++ incognito/src/main/java/com/cout/incognito/controller/incognitoController.java
@@ -1,15 +1,33 @@
 package com.cout.incognito.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.cout.incognito.models.Courses;
+import com.cout.incognito.models.User;
+import com.cout.incognito.repository.CoursesRepo;
+import com.cout.incognito.repository.UserRepo;
 
 @Component
 @Controller 
-public class incognitoController {
+public class incognitoController implements ErrorController {
+	@Autowired
+	public UserRepo userRepo;
+	
+	@Autowired
+	private CoursesRepo coursesrepo;
 	
 	@RequestMapping("/")
 	public String welomcePage(){
@@ -21,11 +39,33 @@ public class incognitoController {
 		return "login";
 	}
 	
-	@RequestMapping(value="/studentDash", method=RequestMethod.GET)
-	public String studentDash(){
-		return "studentDash";
+	@RequestMapping(value="/studentMessageBoard", method=RequestMethod.GET)
+	public String studentMessageBoard(){
+		return "studentMessageBoard";
 	}
 	
+	@RequestMapping(value="/studentDash", method=RequestMethod.GET)
+	public String studentDash(ModelMap model){
+	   Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	   String name = auth.getName();
+	   User userName = userRepo.findByEMAILIgnoreCase(name);
+	   model.addAttribute("name", userName.getNAME());
+	   
+	   return "studentDash";
+	}
+	
+	@RequestMapping(value="/teacherDash", method=RequestMethod.GET)
+	public ModelAndView teacherDash(ModelMap model){
+	   Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	   String name = auth.getName();
+	   User userName = userRepo.findByEMAILIgnoreCase(name);
+	   model.addAttribute("name", userName.getNAME());
+	   List<Courses> courses = (List<Courses>) coursesrepo.findAll();
+	   ModelAndView mv = new ModelAndView("teacherDash"); 
+	   mv.addObject("courses", courses);	    
+		
+	   return mv;
+	}
 	
 	@RequestMapping("/studentSU") 
 	public String studentSignup() {
@@ -38,16 +78,24 @@ public class incognitoController {
 		System.out.println("AppController ->teacher");
 		return "teacherSU";
 	}
-
-	@RequestMapping("/TeacherDash")
-	public String teacherDash(HttpServletRequest request){
-		request.setAttribute("mode","MODE_HOME");
-		return "TeacherDash";
-	}
 	
 	@RequestMapping("/logout")
 	public String logout(){
 		return "logout";
+	}
+
+	@RequestMapping(value="/access-denied", method=RequestMethod.GET)
+	public String accessDenied(){
+		return "access-denied";
+	}
+	@RequestMapping("/error")
+	public String errorPage(){
+		return "error";
+	}
+	
+	@Override
+	public String getErrorPath() {
+		return "/error";
 	}
 
 } 
